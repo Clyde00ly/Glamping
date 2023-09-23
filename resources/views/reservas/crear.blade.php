e4a523e4ac479b16fe89ea00905c3d31288fe240
Cami <133257394+kami5214@users.noreply.github.com>
2023-09-22 20:17:00 -0500

    Update crear.blade.php

--- resources/views/reservas/crear.blade.php
+++ resources/views/reservas/crear.blade.php
@@ -111,6 +111,17 @@
 @enderror 
 </div>
     </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+            <label for="met_codigo">Metodo de pago:</label>
+            <select class="form-control" id="met_codigo" name="met_codigo">
+                <option value="">Seleccionar un metodo de pago</option>
+                @foreach($metodos as $metodo)
+                    <option value="{{ $metodo->met_codigo }}">{{ $metodo->met_nombre }}</option>
+                @endforeach
+            </select>
+        </div>
+            </div>
 
     
     <div class="col-xs-12 col-sm-12 col-md-12">
