d23ef625796afe1566ac2ee3f89f4cde37e92e61
Cami <133257394+kami5214@users.noreply.github.com>
2023-09-22 19:24:14 -0500

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -81,6 +81,6 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
                              ])->assignRole('administrador'); 
 
-        ]);
+        
     }
 }
